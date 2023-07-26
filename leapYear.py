de8ad0106d7f3d8fbcb3e7a5022780e60f62f15e
Rayyanhdo <rayyanalsbah@gmail.com>
2023-07-26 16:26:36 +0400

    add

---
+++ leapYear.py
@@ -0,0 +1,13 @@
+year = int(input("enter the year"))
+
+if (year%4==0) and (year%100==0):
+    print(year, "is leap year")
+elif (year%400==0) and (year%100==0):
+          print(year, "is leap year")
+else:
+        print(year, "is not leap year")
+        
+  
+               
+               
+          
